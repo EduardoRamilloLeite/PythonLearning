44300a32ff7480ab86d257e3925276155fe57e4e
EduardoRamilloLeite <eduardoramilloleite@leite.com>
2024-02-23 22:31:04 -0300

    aulas

---
+++ project/aula15.py
@@ -0,0 +1,7 @@
+entrada = input('voce quer entrar ou sair?')
+if entrada == 'entrar':
+    print('vc entrou no sistema')
+elif entrada == 'sair':
+    print('vc saiu do sistema')
+else:
+    print('vc nao digitou nem entrar e nem sair:')
