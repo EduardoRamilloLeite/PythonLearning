da07a1d6cecbc39008e45e813d0c9434844f9c9b
EduardoRamilloLeite <eduardoramilloleite@leite.com>
2024-02-25 09:47:33 -0300

    aula 19/20

---
+++ project/aula19.py
@@ -0,0 +1,9 @@
+#operadores de comparação
+
+maior = 1 > 2
+maior_ou_igual = 2 >= 2
+menor = 1 < 2
+menor_ou_igual = 2 <= 2
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+print(maior_ou_igual)
---
+++ project/aula20.py
@@ -0,0 +1,7 @@
+primeiroValor = input('digite um valor: ')
+segundoValor = input('digite outro valor: ')
+
+if primeiroValor >= segundoValor:
+    print( f'{primeiroValor} é maior ou igual ao {segundoValor} ')
+else:
+    print(f'{segundoValor} é maior do que o {primeiroValor} ')
