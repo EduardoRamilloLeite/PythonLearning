9aa4e93bbc32d3c88683b71bf81784c9105aa6ec
EduardoRamilloLeite <eduardoramilloleite@leite.com>
2024-02-27 18:13:04 -0300

    aula 21

---
+++ project/aula21.py
@@ -0,0 +1,12 @@
+# entrada = input('[E]ntrar [S]air')
+# print (entrada)
+
+# senha_digitada = input('Senha:')
+# senha_permitida = '123456'
+
+# if entrada == 'E' and senha_digitada == senha_permitida:
+#     print('Entrar')
+# else:
+#     print('Sair')
+
+print (True and False and True)
